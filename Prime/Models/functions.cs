12f33768a6e323e67a752dca5bd115ef55af8ee3
josephjacksonw <josephjacksonw@gmail.com>
2022-09-21 14:56:49 -0400

    first test good pass
    Co-Authored-By: Noah Lundquist <nalundquist@gmail.com>

--- Prime/Models/functions.cs
+++ Prime/Models/functions.cs
@@ -7,28 +7,30 @@ namespace Prime.Models
   {
     public string showPrimes(int userInput)
     {
-      return "false";
-      // List<int> outputList = new List<int> {};
-      // for (int i = 2; i <= userInput; i++)
-      // {
-      //   i += 1;
-      // }
+      List<int> outputList = new List<int> {};
+      for (int i = 2; i <= userInput; i++)
+      {
+        if (i==2 || i==3 || i==5 || i==7 || i==11)
+				{
+					outputList.Add(i);
+				}
+				else
+				{
+					if (i%2!=0 && i%3!=0 && i%5!=0 && i%7!=0 && i%11!=0)
+					{
+						outputList.Add(i);
+					}
+				}
+      }
+			string outputString = "";
+      for (int i=0; i < outputList.Count; i++) {
+        outputString += outputList[i];
+        if (i != outputList.Count - 1)
+        {
+          outputString += ", ";
+        }
+      }
+      return outputString;
     }
-
-
-    // for (int i = 2; i < u; i++)
-    // {
-    //   if (i==2 ||3||5||7 etc)
-    //     {add to list}
-    //   else 
-    //   {
-    //     if (i%2 == 0 || i%3 == 0 || all the way up as far as we want, only putting primes in))
-    //     {
-    //       no
-    //     }
-    //     else
-    //     {add to list}
-    //   }
-    // }
   }
 }
